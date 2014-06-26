947f0e974d22efad8ed4c987fbfe289dac75ecf4
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-06-26 12:15:20 +1000

    Mostly style

--- src/BanzaiDB/converters.py
+++ src/BanzaiDB/converters.py
@@ -1,8 +1,35 @@
+# Copyright 2013-2014 Mitchell Stanton-Cook Licensed under the
+# Educational Community License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may
+# obtain a copy of the License at
+#
+# http://www.osedu.org/licenses/ECL-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 import tablib
 import json
+import sys
 
-def convert_from_JSON_to_CSV(json_data, header=False, fmt='csv'): 
+"""
+converters
+==========
+
+Converts data representation formats from one to another.
+"""
+
+
+def convert_from_JSON_to_CSV(json_data, header=False, fmt='csv'):
     """
+    Converts JSON to CSV
+
+    :param json_data: JSON object
+    :param header: [optional]
+    :param fmt: [optional] only csv.
     """
     json_str = json.dumps(json_data)
     supported = ['csv']
@@ -17,7 +44,9 @@ def convert_from_JSON_to_CSV(json_data, header=False, fmt='csv'):
     else:
         raise Exception("Only output in CSV is supported")
 
+
 def convert_from_csv_to_JSON(csv_data, header):
     """
     """
-    data = tablib.Dataset()
+    print "NotImplemented yet!"
+    sys.exit(1)
