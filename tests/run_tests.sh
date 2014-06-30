44cb2a479f109f5798b9c31dd29dd2baf3aa9908
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-06-30 13:03:10 +1000

    Test runner

---
+++ tests/run_tests.sh
@@ -0,0 +1,10 @@
+python test_banzaidb.py
+python test_config.py
+python test_converters.py
+python test_core.py
+python test_database.py
+python test_fetch.py
+python test_imaging.py
+python test_misc.py
+python test_parsers.py
+python test_query_functions.py
