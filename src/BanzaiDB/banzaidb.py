c511b22172d9c3341b9b216a00b7f5ef288d950c
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-07-03 14:24:00 +1000

    Slight change to make the CLI testing easier

--- src/BanzaiDB/banzaidb.py
+++ src/BanzaiDB/banzaidb.py
@@ -245,7 +245,7 @@ def create_parser():
     populate_p.set_defaults(func=populate_database_with_data)
     update_p.set_defaults(func=updateDB)
     query_p.set_defaults(func=db_query)
-    return parser
+    return parser.parse_args()
 
 
 def main():
@@ -254,8 +254,7 @@ def main():
     """
     try:
         start_time = time.time()
-        parser = create_parser()
-        args = parser.parse_args()
+        args = create_parser()
         if args.verbose:
             print "Executing @ " + time.asctime()
         args.func(args)
