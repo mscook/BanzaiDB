17e25d10026345e6e4a5d505762cd409875e3bc2
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-07-08 11:06:15 +1000

    Mainly style fixes

--- src/BanzaiDB/imaging.py
+++ src/BanzaiDB/imaging.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Mitchell Stanton-Cook Licensed under the
+# Copyright 2013-2014 Mitchell Stanton-Cook Licensed under the
 # Educational Community License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may
 # obtain a copy of the License at
@@ -13,42 +13,42 @@
 
 
 import random
-from   reportlab.lib import colors
-from   reportlab.lib.units import cm
-from   Bio.Graphics import GenomeDiagram
-
+from reportlab.lib.units import cm
+from Bio.Graphics import GenomeDiagram
 
 
 SEED = 99
 
+
 def hsv_to_rgb(h, s, v):
     """
     Convert HSV to RGB
-    
+
     :param h: hue
     :param s: saturation
     :param v: value
     """
     h_i = int(h*6)
-    f   = h*6 - h_i
-    p   = v * (1 - s)
-    q   = v * (1 - f*s)
-    t   = v * (1 - (1 - f) * s)
-    if h_i == 0: 
+    f = h*6 - h_i
+    p = v * (1 - s)
+    q = v * (1 - f*s)
+    t = v * (1 - (1 - f) * s)
+    if h_i == 0:
         r, g, b = v, t, p
-    elif h_i == 1: 
+    elif h_i == 1:
         r, g, b = q, v, p
-    elif h_i == 2: 
+    elif h_i == 2:
         r, g, b = p, v, t
-    elif h_i == 3: 
+    elif h_i == 3:
         r, g, b = p, q, v
-    elif h_i == 4: 
+    elif h_i == 4:
         r, g, b = t, p, v
-    elif h_i == 5: 
+    elif h_i == 5:
         r, g, b = v, p, q
     else:
         print "Problem"
     return [int(r*256), int(g*256), int(b*256)]
+
 
 def gen_colors(number):
     """
@@ -56,10 +56,10 @@ def gen_colors(number):
 
     Based on http://martin.ankerl.com/
         2009/12/09/how-to-create-random-colors-programmatically/
-    
+
     :param number: int
     :type: int
-    :rtype: a list of lists in the form: [[243, 137, 121], [232, 121, 243], 
+    :rtype: a list of lists in the form: [[243, 137, 121], [232, 121, 243],
                                           [216, 121, 243]]
     """
     rgb_list = []
@@ -73,15 +73,15 @@ def gen_colors(number):
 
 
 def plot_SNPs(snp_features, labels):
+    """
+    Using GenomeDiagram from Biopython generate a SNP position plot_SNPs
 
+    """
     gdd = GenomeDiagram.Diagram('SNP Diagram')
-    for idx, track in enumerate(snp_features): 
+    for idx, track in enumerate(snp_features):
         gdt_features = gdd.new_track(idx+1, greytrack=False, name=labels[idx])
         gds_features = gdt_features.new_set()
         for f in track:
-            #print f
-            #exit()
             gds_features.add_feature(f)
-    gdd.draw(format='circular', pagesize=(15*cm,15*cm), track_size=0.25)
+    gdd.draw(format='circular', pagesize=(15*cm, 15*cm), track_size=0.25)
     gdd.write("GD_labels_default.pdf", "pdf")
-
