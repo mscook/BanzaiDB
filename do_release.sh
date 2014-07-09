5759985a66acd854e52456e3ce49fb8ec06f8647
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-07-09 10:17:00 +1000

    More do-release implementations

--- do_release.sh
+++ do_release.sh
@@ -6,9 +6,26 @@ VERSION=0.1.1
 # Clean, test, build the source distribution & pip install it
 # Need to get exit statuses here...
 python setup.py clean
+
 python setup.py test
+STATUS=`echo $?`
+if [ $STATUS -eq 0 ]; then
+    echo ""
+else
+    echo "Tests failed. Will not release"
+    exit
+fi 
+
 python setup.py sdist
 pip install dist/BanzaiDB-$VERSION.tar.gz
+STATUS=`echo $?`
+if [ $STATUS -eq 0 ]; then
+    echo ""
+else
+    echo "Package is not pip installable. Will not release"
+    exit
+fi 
+
 
 # Docs
 # Need to get exit statuses here...
@@ -20,6 +37,7 @@ rmdir API
 make html
 
 # tag & push the tag to github
+GIT=`git status`
 
 # Upload to PyPI
 
